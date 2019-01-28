fa5d6b7c12d66c3a7a79dc0839e2d6480484f3d6
eileenloves <eileenkim1208@gmail.com>
2019-01-27 23:35:58 -0500

    Basic JS func

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-const title = document.getElementById("title");
-title.innerHTML = "Hi! From JS";
+const title = document.getElementById("#title");
+
+const CLICKED_CLASS="clicked";
+
+function handleClick(){
+  title.classList.toggle(CLICKED_CLASS);
+  // const hasClass = title.classList.contains(CLICKED_CLASS);
+  // if(hasClass){
+  //   title.classList.remove(CLICKED_CLASS);
+  // } else {
+  //   title.classList.add(CLICKED_CLASS);
+  // }
+}
+
+function init(){
+  title.addEventListener("click",handleClick);
+}
+init();
